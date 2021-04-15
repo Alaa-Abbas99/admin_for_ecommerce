0150d0850c279d08a527ee21cdae326b5689ab59
Alaa <alaabasim39@gmail.com>
2021-04-16 00:08:40 +0300

    Removing unecesssary files

--- laravel-admin/app/Http/Controllers/AuthController.php
+++ laravel-admin/app/Http/Controllers/AuthController.php
@@ -35,7 +35,7 @@ class AuthController extends Controller
     }
 
     public function logout(){
-        $cookie = Cookie::forget('jwt');
+        $cookie = \Cookie::forget('jwt');
 
         return response([
             'message'=>'success',
